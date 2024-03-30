08bfd3532a89cd20e78d7aee51da3b27812d7656
Anshumaan Singh <anshumaanborntolead@gmail.com>
2024-03-30 17:03:59 +0530

    Update update.jsx

--- src/pages/update.jsx
+++ src/pages/update.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Pseudo_Nav } from "../Components/pseudo_nav";
+import { useState } from "react";
+import axios from "axios";
 
 
 const Update=()=> {
+
+  const[book,setbook]=useState({
+    book_name:"",
+    book_description:"",
+    book_author:"" ,
+    book_id:null
+  })
+
+  const handlechange=e=>{
+    setbook(prev=>({...prev,[e.target.name]:e.target.value}))
+  }
+  const handleclick=async e=>{
+    try{
+      await axios.post("http://localhost:8800/update_book",book)
+  
+    }
+    catch(error){
+      console.log(error)
+    }
+  
+  
+  }
+  
+  console.log(book)
+
+
     return (
       <div>
         <Pseudo_Nav/>
@@ -13,18 +41,19 @@ const Update=()=> {
         <h1>Update Book Details</h1>
 
             <div class="row">
-            <div class="col-sm-3 item"><b>Books</b></div>
-            <div class="col-sm-3 item"><b>Add</b></div>
-            <div class="col-sm-3 item"><b>Update</b></div>
-            <div class="col-sm-3 item"><b>Delete</b></div>
+            <div class="col-sm-2 item"><b>Book Name</b></div>
+            <div class="col-sm-3 item"><b>Book Description</b></div>
+            <div class="col-sm-2 item"><b>Book Author</b></div>
+            <div class="col-sm-3 item"><b>ID</b></div>
             </div>
 
             <div class="row">
             <form>
-            <div class="col-sm-3 item"><input type="text" ></input></div>
-            <div class="col-sm-3 item"><input type="text" ></input></div>
-            <div class="col-sm-3 item"><input type="text" ></input></div>
-            <div class="col-sm-3 item"><input type="text" ></input></div>
+            <div class="col-sm-2 item"><input type="text" onChange={handlechange} name="book_name" required></input></div>
+            <div class="col-sm-3 item"><input type="text" onChange={handlechange} name="book_description" required></input></div>
+            <div class="col-sm-2 item"><input type="text" onChange={handlechange} name="book_author" required></input></div>
+            <div class="col-sm-3 item"><input type="text" onChange={handlechange} name="book_id" required></input></div>
+            <div class="col-sm-2 item"><button onClick={handleclick}>Submit</button></div>
             </form>
             </div>
             </div>
